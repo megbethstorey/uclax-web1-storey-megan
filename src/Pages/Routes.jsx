e550e3a99615358c51e9ab9012a076a32c33dfeb
megan storey <megan.storey3@gmail.com>
2024-04-22 19:17:54 -0700

    L4 Complete

--- src/Pages/Routes.jsx
+++ src/Pages/Routes.jsx
@@ -7,6 +7,8 @@ import "@/Common/css/global.scss";
 // layout
 import PagesLayout from "@/Common/PagesLayout/PagesLayout.jsx";
 
+import { MediaQueryProvider } from "@/Common/useMediaQuery.jsx";
+
 // page components
 import Home from "./Home";
 import Staff from "./Staff";
@@ -15,28 +17,33 @@ import Contact from "./Contact";
 // course work page components
 import CourseWorkLayout from "./CourseWork/CourseWorkLayout.jsx";
 import SunAndMoon from "./Coursework/SunAndMoon/SunAndMoon.jsx";
-import Essays from "./Coursework/Essays.jsx";
+import Essays from "./Coursework/Essays/Essays.jsx";
 import ResponsiveDesign from "./Coursework/ResponsiveDesign.jsx";
 
 const MyRoutes = () => {
     return (
         <HashRouter>
-            <Routes>
-                <Route element={<PagesLayout />}>
-                    <Route element={<Home />} path="" />
-                    <Route element={<Staff />} path="staff" />
-                    <Route element={<Contact />} path="contact" />
+            <MediaQueryProvider>
+                <Routes>
+                    <Route element={<PagesLayout />}>
+                        <Route element={<Home />} path="" />
+                        <Route element={<Staff />} path="staff" />
+                        <Route element={<Contact />} path="contact" />
 
-                    <Route element={<CourseWorkLayout />} path="course-work">
-                        <Route element={<Essays />} path="" />
-                        <Route element={<SunAndMoon />} path="" />
                         <Route
-                            element={<ResponsiveDesign />}
-                            path="responsive"
-                        />
+                            element={<CourseWorkLayout />}
+                            path="course-work"
+                        >
+                            <Route element={<Essays />} path="" />
+                            <Route element={<SunAndMoon />} path="" />
+                            <Route
+                                element={<ResponsiveDesign />}
+                                path="responsive"
+                            />
+                        </Route>
                     </Route>
-                </Route>
-            </Routes>
+                </Routes>
+            </MediaQueryProvider>
         </HashRouter>
     );
 };
